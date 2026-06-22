910fef3e57599f26bd70cc17f7eaff96f4120feb
SunnyYYLin <sunnylinyy@outlook.com>
2026-06-23 02:10:56 +0800

    fix: handle async image embeds

--- src/features/figure-caption.ts
+++ src/features/figure-caption.ts
@@ -1,16 +1,45 @@
-import { MarkdownPostProcessorContext, Plugin } from 'obsidian';
+import {
+	MarkdownPostProcessorContext,
+	MarkdownRenderChild,
+	Plugin,
+} from 'obsidian';
 
 const FIGURE_CLASS = 'better-figures-figure';
 const CAPTION_CLASS = 'better-figures-figure-caption';
+const EMBED_SELECTOR = '.internal-embed.image-embed, .image-embed';
 
 export function registerFigureCaption(plugin: Plugin) {
 	plugin.registerMarkdownPostProcessor(
-		(el: HTMLElement, _ctx: MarkdownPostProcessorContext) => {
-			el.querySelectorAll('img').forEach((img) => {
-				processFigureCaption(img);
-			});
+		(el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+			processFigureCaptions(el);
+			ctx.addChild(new FigureCaptionRenderChild(el));
 		},
 	);
+}
+
+class FigureCaptionRenderChild extends MarkdownRenderChild {
+	private observer: MutationObserver | null = null;
+
+	onload() {
+		this.observer = new MutationObserver(() => {
+			processFigureCaptions(this.containerEl);
+		});
+
+		this.observer.observe(this.containerEl, {
+			childList: true,
+			subtree: true,
+		});
+	}
+
+	onunload() {
+		this.observer?.disconnect();
+	}
+}
+
+function processFigureCaptions(el: HTMLElement) {
+	el.querySelectorAll('img').forEach((img) => {
+		processFigureCaption(img);
+	});
 }
 
 export function processFigureCaption(img: HTMLImageElement) {
@@ -42,9 +71,13 @@ export function processFigureCaption(img: HTMLImageElement) {
 }
 
 function getImageRenderTarget(img: HTMLImageElement) {
-	const parent = img.parentElement;
+	const embed = img.closest(EMBED_SELECTOR);
+	if (embed instanceof HTMLElement) {
+		return embed;
+	}
 
-	if (parent?.tagName === 'A' && parent.childElementCount === 1) {
+	const parent = img.parentElement;
+	if (parent?.tagName === 'A') {
 		return parent;
 	}
 
